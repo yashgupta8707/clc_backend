4f7423495dbe06d5c1341fd57c72a2fa2e42f91f
Yashgupta@8707 <yashgupta@spinfluencemedia.com>
2025-11-23 19:15:55 +0530

    Added forms

--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
-const authMiddleware = require('../middleware/auth');
 const Student = require('../models/Student');
 const Contact = require('../models/Contact');
 
